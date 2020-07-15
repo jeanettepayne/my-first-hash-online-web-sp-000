22da69d27f62a5ee71b255bd4d6df854e3a0bdf0
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-15 00:43:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {"dog"=>"Poquito", "husband"=>"Will", "best friend"=>"Courtney"}
 
   
 end
